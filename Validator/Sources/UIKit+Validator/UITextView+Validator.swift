cbf90c0ccff67193e9e16c81123eef034ff98744
Eliseo Juan Quintanilla <v035173@gmail.com>
2019-10-15 15:10:15 +0200

    Update UITextView+Validator.swift

--- Validator/Sources/UIKit+Validator/UITextView+Validator.swift
+++ Validator/Sources/UIKit+Validator/UITextView+Validator.swift
@@ -8,8 +8,8 @@ extension UITextView: ValidatableInterfaceElement {
     open func validateOnInputChange(enabled: Bool) {
         
         switch enabled {        
-        case true: NotificationCenter.default.addObserver(self, selector: #selector(performValidation), name: UITextView.textDidChangeNotification, object: self)
-        case false: NotificationCenter.default.removeObserver(self, name: UITextView.textDidChangeNotification, object: self)
+        case true: NotificationCenter.default.addObserver(self, selector: #selector(performValidation), name: NSNotification.Name.UITextFieldTextDidChange, object: self)
+        case false: NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UITextFieldTextDidChange, object: self)
         }
     }
     
